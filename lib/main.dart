b6e328b670ebc525857e29e3ee11f8fd787d812b
Rafael Joaquim <rafaeljoaquim86@hotmail.com>
2018-12-06 18:30:02 -0200

    Mensagem do commit

---
+++ lib/main.dart
@@ -0,0 +1,217 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+void main() async {
+  //Firestore.instance.collection('mensagens').document('msg5').setData({'from':'joaquim', 'texto':'teste2'});
+
+//  DocumentSnapshot snapshot = await Firestore.instance.collection('mensagens').document('msg2').get();
+//  print(snapshot.data);
+//  QuerySnapshot snapshot = await Firestore.instance.collection('mensagens').getDocuments();
+//  for(DocumentSnapshot doc in snapshot.documents){
+//    print(doc.data) ;
+//  }
+//print(snapshot.documents);
+
+//  Firestore.instance.collection('mensagens').snapshots().listen((snapshot){
+//    for(DocumentSnapshot doc in snapshot.documents) {
+//      print(doc.data);
+//    }
+//  });
+
+  runApp(MyApp());
+}
+
+final ThemeData kIOSTheme = ThemeData(
+    primarySwatch: Colors.orange[500],
+    primaryColor: Colors.lightBlue,
+    primaryColorBrightness: Brightness.light);
+
+final ThemeData kDefaultTheme = ThemeData(
+  primarySwatch: Colors.blueGrey,
+  accentColor: Colors.blueGrey,
+);
+
+final googleSignIn = GoogleSignIn();
+final auth = FirebaseAuth.instance;
+
+Future <Null>  _ensureLoggedIn() async {
+  GoogleSignInAccount user = googleSignIn.currentUser;
+  if (user == null)
+    user = await googleSignIn.signInSilently();
+  if (user == null) user = await googleSignIn.signIn();
+  if (await auth.currentUser() == null) {
+    GoogleSignInAuthentication credentials =
+    await googleSignIn.currentUser.authentication;
+    await auth.signInWithGoogle(
+        idToken: credentials.idToken,
+        accessToken: credentials.accessToken);
+  }
+}
+
+_handleSubmitted(String text) async {
+  await _ensureLoggedIn();
+  _sendMessage(text: text);
+}
+
+void _sendMessage ({String text, String imgUrl}){
+  Firestore.instance.collection('mensagens').add(
+      {
+        'text': text,
+        'imgUrl': imgUrl,
+        'senderName': googleSignIn.currentUser.displayName,
+        'sendrFhotoUrl': googleSignIn.currentUser.photoUrl
+      }
+  );
+}
+
+
+
+
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat Online',
+      debugShowCheckedModeBanner: false,
+      theme: Theme.of(context).platform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kDefaultTheme,
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: false,
+      top: false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Chat App'),
+          centerTitle: true,
+          elevation:
+          Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: ListView(
+                children: <Widget>[ChatMessage(), ChatMessage(), ChatMessage()],
+              ),
+            ),
+            Divider(
+              height: 1.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+              ),
+              child: TextComposer(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TextComposer extends StatefulWidget {
+  @override
+  _TextComposerState createState() => _TextComposerState();
+}
+
+class _TextComposerState extends State<TextComposer> {
+  final _textController = TextEditingController();
+  bool _isComposing = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: Theme.of(context).platform == TargetPlatform.iOS
+            ? BoxDecoration(
+          // ignore: undefined_getter
+            border: Border(top: BorderSide(color: Colors.grey)))
+            : null,
+        child: Row(
+          children: <Widget>[
+            Container(
+              child:
+              IconButton(icon: Icon(Icons.photo_camera), onPressed: () {}),
+            ),
+            Expanded(
+              child: TextField(
+                controller: _textController,
+                decoration:
+                InputDecoration.collapsed(hintText: 'Enviar uma mensagem'),
+                onChanged: (text) {
+                  setState(() {
+                    _isComposing = text.length > 0;
+                  });
+                },
+                onSubmitted: (text){
+                  _handleSubmitted(text);
+                },
+              ),
+            ),
+            Container(
+                margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                child: Theme.of(context).platform == TargetPlatform.iOS
+                    ? CupertinoButton(
+                  child: Text('Enviar'),
+                  onPressed: _isComposing ? () { _handleSubmitted(_textController.text);} : null,
+                )
+                    : IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: _isComposing ? () { _handleSubmitted(_textController.text);} : null,
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+      child: Row(
+        children: <Widget>[
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://cdn4.iconfinder.com/data/icons/dogs-6/300/06-512.png'),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Rafael',
+                  style: Theme.of(context).textTheme.subhead,
+                ),
+                Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: Text('teste'),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
